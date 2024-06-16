721951d60096adffd2e941befcebda327fab581e
Kim Hyeon U <161267082+DSWoodin@users.noreply.github.com>
2024-06-16 20:01:21 +0900

    Delete hrd_AIyolo directory

--- hrd_AIyolo/test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-file_path = 'C:/pythonworkspace/AI_practice/YOLO_Object_Detection'
-
-with open(file_path, 'r', encoding='utf-8') as fp:
-    data = json.load(fp)
-
-print(json.dumps(data, indent=4))
